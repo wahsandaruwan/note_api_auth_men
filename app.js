af78141d2867cc207a61c7b93804ffb5af479463
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-12 16:50:22 +0530

    Code updated

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
-const app = express();
+const app = express()
+
+const PORT = 3300
 
 app.get('/', (req, res) =>{
     res.send("Hello World!")
 })
 
-app.listen(3300)
+// Bind and listen the connections on the specified host and port
+app.listen(3300, () => {
+    console.log(`Server on Port : ${PORT}`)
+})
