0a6c9fa2f063dad7ee4512029cc1fa925f23f9c6
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-13 09:13:32 +0530

    filename, hashpass udated

--- routes/auth.js
+++ routes/auth.js
@@ -8,19 +8,40 @@ const {consValidationErr} = require('../val_errs_cons')
 
 const {emailExistInDB} = require('../validations')
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
+
+const multer = require('multer')
+
+// Image upload
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './uploads/')
+    },
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname)
+    }
+})
+
+const maxSize = 1 * 1024 * 1024
+const uploadImage = multer({
+    storage: storage,
+    limits: {fileSize: maxSize}
+})
 
 // Register router
-router.post('/register', async (req, res) => {
-    // Hashing password
-    const salt_rounds = 8
-    const hashPass = await bcrypt.hash(req.body.password, salt_rounds)
+router.post('/register', uploadImage.single('image'), async (req, res) => {
+    // Data
+    const name = req.body.name
+    const email = req.body.email
+    const hashedPass = req.body.password !== undefined ? await bcrypt.hash(req.body.password, 8) : undefined
+    const profileImage = req.file ? req.file.filename : undefined 
 
     // Create a new user
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPass
+        name: name,
+        email: email,
+        password: hashedPass,
+        profileImage: profileImage
     })
 
     try{
